ea7c27462cf0060935cf3fcb5799214715b27696
alpbeysir95 <alpbeysir95@gmail.com>
2022-09-02 19:32:23 +0300

    Initial commit

---
+++ Filter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+
+abstract class Filter
+{
+    public abstract string Name { get; }
+    public abstract string[] Run(string[] names);
+
+}
+class GlobalFilter : Filter
+{
+    public override string Name => "GlobalFilter";
+
+    readonly string[] extensionBlacklist = { ".exe", ".config", ".pdb" };
+
+    public override string[] Run(string[] names)
+    {
+        List<string> temp = new List<string>(names);
+        for (int i = 0; i < names.Length; i++)
+        {
+            string ext = Path.GetExtension(names[i]);
+            for (int n = 0; n < extensionBlacklist.Length; n++)
+            {
+                if (ext == extensionBlacklist[n])
+                {
+                    temp.Remove(names[i]);
+                }
+            }
+        }
+        return temp.ToArray();
+    }
+}
+class ReplaceFilter : Filter
+{
+    public override string Name => "Değiştirme Filtresi";
+
+    public override string[] Run(string[] names)
+    {
+        Console.WriteLine("Değiştirmek istediğiniz yazıyı girin, ardından Enter'a basın");
+        string target = Console.ReadLine();
+        Console.Write("Girdiğiniz şey ne ile değiştirilsin? Girin ve Enter'a basın\n(Hiçbir şey girmezseniz yukarıda girdiğinizi siler)\n");
+        string source = Console.ReadLine();
+
+        string[] result = new string[names.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = names[i].Replace(target, source);
+        }
+        return result;
+    }
+}
+
+class TVSeriesFilter : Filter
+{
+    public override string Name => "Dizi Filtresi";
+
+    public override string[] Run(string[] names)
+    {
+        Console.WriteLine("Sezon numarası girin");
+        string season = Console.ReadLine();
+        Console.WriteLine("Kalıp girin (Örnek kalıp: \"Fringe {0}. Sezon {1}. Bölüm\")");
+        string kalıp = Console.ReadLine();
+
+        string[] result = new string[names.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = string.Format(kalıp, season, (i + 1));
+        }
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static Filter[] filters = { new ReplaceFilter(), new TVSeriesFilter() };
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.Title = "İsimlendirici";
+
+        #region Filter Selection
+        Console.WriteLine("Filtre seçin");
+        for (int i = 0; i < filters.Length; i++)
+        {
+            Console.WriteLine((i + 1) + ". " + filters[i].Name);
+        }
+
+        int selected;
+
+        if (!int.TryParse(Console.ReadLine(), out selected)) 
+            FilterSelectError();
+
+        if (selected <= 0 || selected > filters.Length)
+            FilterSelectError();
+
+        Filter filter = filters[selected - 1];
+        #endregion Filter Selection
+
+        //Get files
+        string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+        if (files.Length <= 0)
+            Restart();
+
+        //Remove blacklisted files
+        GlobalFilter globalFilter = new GlobalFilter();
+        files = globalFilter.Run(files);
+
+        //Remove extensions
+        string[] exts = new string[files.Length];
+        for (int i = 0; i < files.Length; i++)
+        {
+            exts[i] = Path.GetExtension(files[i]);
+            files[i] = Path.GetFileNameWithoutExtension(files[i]);
+        }
+
+        //Run filter
+        Console.Clear();
+        string[] result = filter.Run(files);
+        Console.Clear();
+
+        //Remove whitespace
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = result[i].Trim();
+        }
+
+        //Display changes
+        for (int i = 0; i < files.Length; i++)
+        {
+            Console.WriteLine(files[i] + " --> " + result[i]);
+        }
+
+        //Confirmation
+        Console.WriteLine();
+        Console.WriteLine("Bu değişiklikleri onaylıyor musunuz? (E - Evet, H - Hayır)");
+        if (Console.ReadLine() == "E")
+        {
+            //Rename
+            for (int i = 0; i < result.Length; i++)
+            {
+                File.Move(files[i] + exts[i], result[i] + exts[i]);
+            }
+        }
+
+        //Loop
+        Restart();
+    }
+
+    static void FilterSelectError()
+    {
+        Console.WriteLine("Düzgün girin!");
+        Console.ReadKey();
+        Restart();
+    }
+    static void Restart()
+    {
+        Console.Clear();
+        Main();
+        Environment.Exit(0);
+    }
+}
